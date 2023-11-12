637fad7af371e2fc8ee79d0b36fde7f2d7677884
kyum-q <109158497+kyum-q@users.noreply.github.com>
2023-11-12 21:28:47 +0900

    feat(예외처리) : 날짜 입력 예외 처리 구현

---
+++ src/main/java/christmas/validator/DateValidator.java
@@ -0,0 +1,52 @@
+package christmas.validator;
+
+import christmas.constant.ErrorMessage;
+import christmas.constant.OrderConstant;
+
+public class DateValidator {
+    private int date;
+    private OrderConstant minDate, maxDate;
+    private ErrorMessage errorMessage;
+
+    public DateValidator() {
+        minDate = OrderConstant.MIN_DATE;
+        maxDate = OrderConstant.MAX_DATE;
+    }
+    public boolean validate(String s) {
+        return validOtherString(s) && validLessMinOverMax(date);
+    }
+
+    private boolean validOtherString(String s) {
+        try {
+            date = Integer.parseInt(s);
+        }
+        catch (IllegalArgumentException e) {
+            errorMessage = ErrorMessage.ERROR_DATE;
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validLessMinOverMax(int date) {
+        try {
+            if(date < minDate.getNumber() || date > maxDate.getNumber()) {
+                throw new IllegalArgumentException();
+            }
+        }
+        catch (IllegalArgumentException e) {
+            errorMessage = ErrorMessage.ERROR_DATE;
+            return false;
+        }
+        return true;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public String getErrorMessage() {
+        if(errorMessage==null)
+            return "";
+        return errorMessage.getMessage();
+    }
+}
