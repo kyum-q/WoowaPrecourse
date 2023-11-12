f95c4b96a6bd941f95037811f2c9b1cb0193e380
kyum-q <109158497+kyum-q@users.noreply.github.com>
2023-11-13 00:41:27 +0900

    feat(메뉴) : 메뉴 클래스 구현

---
+++ src/main/java/christmas/model/Menu.java
@@ -0,0 +1,44 @@
+package christmas.model;
+
+import christmas.constant.*;
+import com.sun.tools.javac.Main;
+
+public class Menu {
+    private final String menuName;
+    private final int size;
+
+    private MenuKind kind;
+
+    public Menu(String menuName, int size) {
+        setMenuKind(menuName);
+
+        if(kind == null)
+            throw new IllegalArgumentException();
+
+        this.menuName = menuName;
+        this.size = size;
+    }
+
+    private void setMenuKind(String menuName) {
+        if(AppetizerMenu.contains(menuName)) {
+            kind = MenuKind.APPETIZER;
+        }
+        if(MainMenu.contains(menuName)) {
+            kind = MenuKind.MAIN;
+        }
+        if(DessertMenu.contains(menuName)) {
+            kind = MenuKind.DESSERT;
+        }
+        if(DrinkMenu.contains(menuName)) {
+            kind = MenuKind.DRINK;
+        }
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public MenuKind getKind() {
+        return kind;
+    }
+}
