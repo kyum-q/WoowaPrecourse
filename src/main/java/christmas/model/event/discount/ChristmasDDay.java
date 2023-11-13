332ef79da5477f48d4cc6b65f78508b1d0d259f9
kyum-q <109158497+kyum-q@users.noreply.github.com>
2023-11-14 01:55:11 +0900

    style(클래스명) : 크리스마스 디데이 이벤트 클래스 명 수정

--- src/main/java/christmas/model/event/discount/ChristmasDDay.java
+++ src/main/java/christmas/model/event/discount/ChristmasDDay.java
@@ -4,11 +4,11 @@ import christmas.constant.event.EventConstant;
 import christmas.constant.event.EventName;
 import christmas.model.event.Event;
 
-public class ChrismasDday extends Event {
-    ChrismasDday(int date) {
+public class ChristmasDDay extends Event {
+    public ChristmasDDay(int date) {
         int init = EventConstant.D_DAY_DISCOUNT_INIT_VALUE.value();
         int increase = EventConstant.D_DAY_INCREASE_VALUE.value();
-        discountPrice = init + increase * date;
+        discountPrice = init + increase * (date - 1);
 
         eventName = EventName.EVENT_D_DAY;
     }
