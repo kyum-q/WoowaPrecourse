4b4acb390a964cfc7a1a1274630e7f7f36f806f1
kyum-q <109158497+kyum-q@users.noreply.github.com>
2023-11-14 01:58:44 +0900

    refactor(에러메시지) : 에러메시지 출력 수정

--- src/main/java/christmas/constant/ErrorMessage.java
+++ src/main/java/christmas/constant/ErrorMessage.java
@@ -1,8 +1,8 @@
 package christmas.constant;
 
 public enum ErrorMessage {
-    ERROR_DATE(OrderConstant.MIN_DATE.getNumber() + "-" + OrderConstant.MAX_DATE.getNumber() + " 숫자 중에 입력해주세요."),
-    ERROR_ORDER_OTHER_DATE("메뉴 입력 형식을 지켜주세요. (e.g. 시저샐러드-1, 티본스테이크-1)"),
+    ERROR_DATE("유효하지 않은 날짜입니다."),
+    ERROR_ORDER_OTHER_DATE("유효하지 않은 주문입니다."),
     ERROR_ORDER_NOT_EXIST_MENU("없는 메뉴는 주문할 수 없습니다."),
     ERROR_OVER_MAX_SIZE("메뉴는 최대 " + OrderConstant.MAX_ORDER.getNumber() + "개까지만 주문 가능합니다."),
     ERROR_OVER_ONLY_DRINK("음료만 주문하실 수 없습니다.");
@@ -14,6 +14,6 @@ public enum ErrorMessage {
     }
 
     public String getMessage() {
-        return message;
+        return "[ERROR] " + message + " 다시 입력해 주세요.";
     }
 }
