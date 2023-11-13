ce92380e5f860aa073d9466bc5c059f9c1ab0352
kyum-q <109158497+kyum-q@users.noreply.github.com>
2023-11-13 20:38:13 +0900

    feat(이벤트) : 평일, 주말 이벤트 클래스 구현

---
+++ src/main/java/christmas/model/event/discount/Week.java
@@ -0,0 +1,40 @@
+package christmas.model.event.discount;
+
+import christmas.constant.event.EventConstant;
+import christmas.constant.event.EventName;
+import christmas.constant.menu.MenuKind;
+import christmas.model.Menu;
+import christmas.model.event.Event;
+
+import java.util.List;
+
+
+public class Week extends Event {
+    private final boolean isWeekend;
+    public Week(int date, List<Menu> menus) {
+        isWeekend = checkWeekends(date);
+        discountCost = getDiscountCost(menus);
+    }
+
+    private boolean checkWeekends(int date) {
+        int week = getWeek(date);
+
+        if(week==6 || week==7) {
+            eventName = EventName.EVENT_WEEKEND;
+            return true;
+        }
+        eventName = EventName.EVENT_WEEKDAY;
+        return false;
+    }
+
+    private int getDiscountCost(List<Menu> menus) {
+        if(isWeekend) {
+            int mainOrderSize = menus.stream().filter(menu -> menu.getKind() == MenuKind.MAIN)
+                    .mapToInt(Menu::getSize).sum();
+            return mainOrderSize * EventConstant.WEEK_DISCOUNT_VALUE.value();
+        }
+        int mainOrderSize = menus.stream().filter(menu -> menu.getKind() == MenuKind.DESSERT)
+                .mapToInt(Menu::getSize).sum();
+        return mainOrderSize * EventConstant.WEEK_DISCOUNT_VALUE.value();
+    }
+}
