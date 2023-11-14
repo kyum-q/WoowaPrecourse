6c5459206c90771a02dd736719f6e1cc9a8c7ba9
kyum-q <109158497+kyum-q@users.noreply.github.com>
2023-11-15 02:44:34 +0900

    style(코드 정리) : 불필요한 코드 정리

--- src/main/java/christmas/controller/OrderController.java
+++ src/main/java/christmas/controller/OrderController.java
@@ -1,16 +1,11 @@
 package christmas.controller;
 
-import christmas.constant.OrderConstant;
-import christmas.constant.event.EventConstant;
-import christmas.constant.menu.DrinkMenu;
 import christmas.model.EventManager;
 import christmas.model.Menu;
 import christmas.model.OrderCalculate;
-import christmas.model.event.Event;
 import christmas.view.Input;
 import christmas.view.Output;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class OrderController {
@@ -21,7 +16,6 @@ public class OrderController {
     private int date;
     private List<Menu> orderMenus;
     private int beforeDiscountTotalPrice, benefitPrice;
-    private List<Event> events;
 
     public OrderController() {
         input = new Input();
@@ -52,9 +46,6 @@ public class OrderController {
         output.printTotalBenefit(benefitPrice);
         output.printAfterDiscountTotalPrice(calculate.getAfterDiscountTotalPrice
                 (eventManager.isGiftEvent(),beforeDiscountTotalPrice,benefitPrice));
-        output.printGiftBadge(eventManager.getBadge());
+        output.printGiftBadge(eventManager.getBadgeName());
     }
-
-
-
 }
