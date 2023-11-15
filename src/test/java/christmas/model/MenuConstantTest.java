b96273823674f2094de0b3f515a2569eef50f93c
kyum-q <109158497+kyum-q@users.noreply.github.com>
2023-11-15 16:41:19 +0900

    test(메뉴 확인) : 메뉴 확인 테스트 수정

--- src/test/java/christmas/model/MenuConstantTest.java
+++ src/test/java/christmas/model/MenuConstantTest.java
@@ -18,83 +18,84 @@ class MenuConstantTest {
     void 에피타이저_테스트() {
         Menu menu = new Menu("양송이수프", size);
         int price = menu.getPrice();
-        MenuKind kind = menu.getKind();
         assertThat(price).isEqualTo(6000);
-        assertThat(kind).isEqualTo(MenuKind.APPETIZER);
+        boolean isKind = menu.checkKind(MenuKind.APPETIZER);
+        assertThat(isKind).isEqualTo(true);
 
         menu = new Menu("타파스", size);
         price = menu.getPrice();
-        kind = menu.getKind();
         assertThat(price).isEqualTo(5500);
-        assertThat(kind).isEqualTo(MenuKind.APPETIZER);
+        isKind = menu.checkKind(MenuKind.APPETIZER);
+        assertThat(isKind).isEqualTo(true);
 
         menu = new Menu("시저샐러드", size);
         price = menu.getPrice();
-        kind = menu.getKind();
         assertThat(price).isEqualTo(8000);
-        assertThat(kind).isEqualTo(MenuKind.APPETIZER);
+        isKind = menu.checkKind(MenuKind.APPETIZER);
+        assertThat(isKind).isEqualTo(true);
     }
 
     @Test
     void 메인_테스트() {
         Menu menu = new Menu("티본스테이크", size);
         int price = menu.getPrice();
-        MenuKind kind = menu.getKind();
         assertThat(price).isEqualTo(55000);
-        assertThat(kind).isEqualTo(MenuKind.MAIN);
+        boolean isKind = menu.checkKind(MenuKind.MAIN);
+        assertThat(isKind).isEqualTo(true);
 
         menu = new Menu("바비큐립", size);
         price = menu.getPrice();
-        kind = menu.getKind();
         assertThat(price).isEqualTo(54000);
-        assertThat(kind).isEqualTo(MenuKind.MAIN);
+        isKind = menu.checkKind(MenuKind.MAIN);
+        assertThat(isKind).isEqualTo(true);
 
         menu = new Menu("해산물파스타", size);
         price = menu.getPrice();
-        kind = menu.getKind();
         assertThat(price).isEqualTo(35000);
-        assertThat(kind).isEqualTo(MenuKind.MAIN);
+        isKind = menu.checkKind(MenuKind.MAIN);
+        assertThat(isKind).isEqualTo(true);
 
         menu = new Menu("크리스마스파스타", size);
         price = menu.getPrice();
-        kind = menu.getKind();
         assertThat(price).isEqualTo(25000);
-        assertThat(kind).isEqualTo(MenuKind.MAIN);
+        isKind = menu.checkKind(MenuKind.MAIN);
+        assertThat(isKind).isEqualTo(true);
     }
 
     @Test
     void 디저트_테스트() {
         Menu menu = new Menu("초코케이크", size);
         int price = menu.getPrice();
-        MenuKind kind = menu.getKind();
         assertThat(price).isEqualTo(15000);
-        assertThat(kind).isEqualTo(MenuKind.DESSERT);
+        boolean isKind = menu.checkKind(MenuKind.DESSERT);
+        assertThat(isKind).isEqualTo(true);
 
         menu = new Menu("아이스크림", size);
         price = menu.getPrice();
-        kind = menu.getKind();
         assertThat(price).isEqualTo(5000);
-        assertThat(kind).isEqualTo(MenuKind.DESSERT);
+        isKind = menu.checkKind(MenuKind.DESSERT);
+        assertThat(isKind).isEqualTo(true);
     }
 
     @Test
     void 음료_테스트() {
         Menu menu = new Menu("제로콜라", size);
         int price = menu.getPrice();
-        MenuKind kind = menu.getKind();
         assertThat(price).isEqualTo(3000);
-        assertThat(kind).isEqualTo(MenuKind.DRINK);
+        boolean isKind = menu.checkKind(MenuKind.DRINK);
+        assertThat(isKind).isEqualTo(true);
 
         menu = new Menu("레드와인", size);
         price = menu.getPrice();
-        kind = menu.getKind();
         assertThat(price).isEqualTo(60000);
-        assertThat(kind).isEqualTo(MenuKind.DRINK);
+        isKind = menu.checkKind(MenuKind.DRINK);
+        assertThat(isKind).isEqualTo(true);
+
 
         menu = new Menu("샴페인", size);
         price = menu.getPrice();
-        kind = menu.getKind();
         assertThat(price).isEqualTo(25000);
-        assertThat(kind).isEqualTo(MenuKind.DRINK);
+        isKind = menu.checkKind(MenuKind.DRINK);
+        assertThat(isKind).isEqualTo(true);
     }
 }
