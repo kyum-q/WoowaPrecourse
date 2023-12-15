bd69c43c774ac42e84875698c9a114899ebf348b
KyungMi <109158497+kyum-q@users.noreply.github.com>
2023-12-16 02:01:50 +0900

    feat(컨트롤러): 컨트롤러 구현

--- src/main/java/menu/controller/MenuRecommendController.java
+++ src/main/java/menu/controller/MenuRecommendController.java
@@ -1,11 +1,16 @@
 package menu.controller;
 
+import menu.model.Coaches;
+import menu.model.RecommendMenu;
 import menu.view.InputView;
 import menu.view.OutputView;
+
+import java.util.List;
 
 public class MenuRecommendController {
     private OutputView outputView;
     private InputView inputView;
+    private RecommendMenu recommendMenu;
 
     public MenuRecommendController() {
         outputView = new OutputView();
@@ -14,7 +19,19 @@ public class MenuRecommendController {
 
     public void startRecommend() {
         outputView.printServiceStartMessage();
-        inputView.requestCoachNames();
+        recommendMenu = new RecommendMenu(settingCoach());
+        recommendMenu.recommend();
+        outputView.printRecommendResult(recommendMenu);
+    }
+
+    public Coaches settingCoach() {
+        Coaches coaches = new Coaches();
+        List<String> coach = inputView.requestCoachNames();
+        for (int i = 0; i < coach.size(); i++) {
+            List<String> dontEat = inputView.requestCoachDontEat(coach.get(i));
+            coaches.add(coach.get(i), dontEat);
+        }
+        return coaches;
     }
 
 }
