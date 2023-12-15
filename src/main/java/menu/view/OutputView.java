e546672de4d77ec7773ce26172cf577f49836d9f
KyungMi <109158497+kyum-q@users.noreply.github.com>
2023-12-16 02:01:36 +0900

    feat(최종 출력) : 최종 추천 결과 출력 구현

--- src/main/java/menu/view/OutputView.java
+++ src/main/java/menu/view/OutputView.java
@@ -1,10 +1,18 @@
 package menu.view;
 
 import menu.constants.PrintMessage;
+import menu.model.RecommendMenu;
 
 public class OutputView {
     public void printServiceStartMessage() {
         System.out.println(PrintMessage.PRINT_START_WORD);
         System.out.println();
+    }
+
+    public void printRecommendResult(RecommendMenu recommendMenu) {
+        System.out.println(PrintMessage.PRINT_ENDING_WORD_START);
+        System.out.println(PrintMessage.PRINT_ENDING_WORD_CATEGORY);
+        System.out.println(recommendMenu.toString());
+        System.out.println(PrintMessage.PRINT_ENDING_WORD_LAST);
     }
 }
