d70a97fdb5c381327a9b4ef6e15e5f87bb281eb2
KyungMi <109158497+kyum-q@users.noreply.github.com>
2023-12-15 23:58:39 +0900

    feat(출력) : 서비스 시작 문구 출력 구현

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,10 @@
+package menu.view;
+
+import menu.constants.PrintMessage;
+
+public class OutputView {
+    public void printServiceStartMessage() {
+        System.out.println(PrintMessage.PRINT_START_WORD);
+        System.out.println();
+    }
+}
