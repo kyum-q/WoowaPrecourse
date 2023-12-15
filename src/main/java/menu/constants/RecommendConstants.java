6f9f12e5f3d2619e1c1362eb8e1d97c6e9348e8b
KyungMi <109158497+kyum-q@users.noreply.github.com>
2023-12-16 01:59:26 +0900

    feat(enum 상수화) : 추천에 필요한 값들 상수화

--- src/main/java/menu/constants/RecommendConstants.java
+++ src/main/java/menu/constants/RecommendConstants.java
@@ -4,7 +4,9 @@ public enum RecommendConstants {
     COACH_MIN_COUNT(2),
     COACH_MAX_COUNT(5),
     COACH_MIN_NAME_LEN(2),
-    COACH_MAX_NAME_LEN(4);
+    COACH_MAX_NAME_LEN(4),
+    DONT_EAT_MENU_MIN(0),
+    DONT_EAT_MENU_MAX(2);
 
     private int number;
     RecommendConstants(int n) {
