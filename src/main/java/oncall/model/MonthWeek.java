88e3b8adbeac70b6086ccef337211b6fe80916c1
KyungMi <109158497+kyum-q@users.noreply.github.com>
2023-12-16 16:00:30 +0900

    feat(날짜 관리) : 날짜 관리 구현 (다음날로 이동, 공휴일 확인, 출력))

--- src/main/java/oncall/model/MonthWeek.java
+++ src/main/java/oncall/model/MonthWeek.java
@@ -1,12 +1,42 @@
 package oncall.model;
 
+import oncall.constants.Holiday;
+import oncall.constants.MonthDay;
 import oncall.constants.Week;
 
 public class MonthWeek {
-    private int month;
+    private int month, day;
     private Week week;
+    private MonthDay monthDay;
+    private boolean isWeeklyHoliday = false;
     public MonthWeek(int month, Week week) {
         this.month = month;
+        this.day = 1;
         this.week = week;
+
+        monthDay = MonthDay.values()[month-1];
+    }
+
+    public boolean next() {
+        if(day >= monthDay.getNum())
+            return false;
+        day++;
+        week = week.nextWeek();
+        return true;
+    }
+
+    public boolean checkHoliday() {
+        isWeeklyHoliday = false;
+        if(week.toString().equals("토") || week.toString().equals("일"))
+            return true;
+        isWeeklyHoliday = Holiday.isHoliday(month, day);
+        return isWeeklyHoliday;
+    }
+
+    @Override
+    public String toString() {
+        if(isWeeklyHoliday)
+            return month+"월 "+day +"일 "+week + "(휴일)";
+        return month+"월 "+day +"일 "+week;
     }
 }
